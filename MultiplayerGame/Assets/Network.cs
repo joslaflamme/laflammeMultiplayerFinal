2953dd18f669af639081e0d38dafe9506f8dfe30
Dubreuill <JDUBREUIL@neit.edu>
2020-02-26 13:08:55 -0500

    Wednesdays class added Movement

--- MultiplayerGame/Assets/Network.cs
+++ MultiplayerGame/Assets/Network.cs
@@ -9,6 +9,7 @@ public class Network : MonoBehaviour
 
     static SocketIOComponent socket;
     public GameObject playerPrefab;
+    Dictionary<string, GameObject> players;
 
 
     // Start is called before the first frame update
@@ -19,11 +20,16 @@ public class Network : MonoBehaviour
         socket.On("open", OnConnected);
         socket.On("spawn", OnSpawned);
         socket.On("move", OnMoved);
+        players = new Dictionary<string, GameObject>();
     }
 
     void OnSpawned(SocketIOEvent e)
     {
-        Instantiate(playerPrefab);
+
+        var player = Instantiate(playerPrefab);
+        Debug.Log("Spawned" + e.data);
+        players.Add(e.data["id"].ToString(), player);
+        Debug.Log(players.Count);
     }   
 
     void OnConnected(SocketIOEvent e)
@@ -37,7 +43,19 @@ public class Network : MonoBehaviour
 
     void OnMoved(SocketIOEvent e)
     {
+        
+        var id = e.data["id"].ToString();
+        var player = players[id];
+        Debug.Log(player.name);
+        var pos = new Vector3(GetFloatFromJson(e.data,"x"), 0, GetFloatFromJson(e.data, "z"));
+        var navigatePos = player.GetComponent<NavigatePos>();
+        navigatePos.NavigateTo(pos);
         Debug.Log("Network player is moving" + e.data);
+    }
+
+    float GetFloatFromJson(JSONObject data, string key)
+    {
+        return float.Parse(data[key].ToString().Replace("\"",""));
     }
 
    
--- MultiplayerServer/app.js
+++ MultiplayerServer/app.js
@@ -5,26 +5,36 @@ var shortid = require("shortid");
 
 
 console.log('Server connected');
-var players = 0;
+var players = [];
+var playerCount = 0;
 io.on('connection', function(socket){
     console.log('client connected');
-    
+
+    var thisClientId = shortid.generate();
+    players.push(thisClientId);
+    playerCount++;
+
     //spawn all newly joined players
-    socket.broadcast.emit('spawn');
-    players++;
+    socket.broadcast.emit('spawn', {id:thisClientId});
+   
 
-    for(var i = 0; i<players; i++){
-        //spawns player character
-        socket.emit('spawn');
-        console.log('spawning player');
-    }
+    players.forEach(function(playerId){
 
+        if(playerId == thisClientId){
+            return;
+        }
+
+        socket.emit('spawn', {id:playerId});
+        console.log('spawning player of id: ', playerId);
+    });
+    
     socket.on('yolo', function(data){
         console.log('You only yolo yolo');
         console.log(data);
     });
 
     socket.on('move', function(data){
+        data.id = thisClientId;
         console.log("player is moving", JSON.stringify(data));
         socket.broadcast.emit('move', data);
     });
